d761b81201ebf64e065e5f6c20262879bfeed0ca
Cristian Sansão <cristian.syb@gmail.com>
2026-03-03 21:10:02 -0300

    adicionando favicon

--- DESAFIOS/desafio002/js/script.js
+++ DESAFIOS/desafio002/js/script.js
@@ -1,27 +1,27 @@
 const card = document.querySelectorAll("article")
-        
-        fetch('data/posts.json')
-            .then(res => res.json())
-            .then(dados =>
-                dados.forEach((post, i) => {
-                    const artigo = card[i]
-                    
-                    artigo.querySelector('h2').textContent = post.titulo
-                    artigo.querySelector('p').textContent = post.descricao
 
-                    const img = document.createElement('img')
-                    img.src = `img/${post.imagem}`
-                    img.alt = post.alt
-                    img.classList.add('img')
-                    artigo.appendChild(img)
+fetch('data/posts.json')
+    .then(res => res.json())
+    .then(dados =>
+        dados.forEach((post, i) => {
+            const artigo = card[i]
 
-                    const link = document.createElement('a')
-                    link.href = `pages/about-world/${post.link}`
-                    link.target = "_self"
+            artigo.querySelector('h2').textContent = post.titulo
+            artigo.querySelector('p').textContent = post.descricao
 
-                    link.setAttribute('aria-label', `Leia a notícia completa sobre ${post.titulo}`)
-                    link.textContent = `Leia mais`
+            const img = document.createElement('img')
+            img.src = `img/${post.imagem}`
+            img.alt = post.alt
+            img.classList.add('img')
+            artigo.appendChild(img)
 
-                    artigo.appendChild(link)
-                })
-            )
+            const link = document.createElement('a')
+            link.href = `pages/about-world/${post.link}`
+            link.target = "_self"
+
+            link.setAttribute('aria-label', `Leia a notícia completa sobre ${post.titulo}`)
+            link.textContent = `Leia mais`
+
+            artigo.appendChild(link)
+        })
+    )
